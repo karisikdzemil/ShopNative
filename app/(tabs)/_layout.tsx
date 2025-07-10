42bc37776e1dfd60d423f18f50eae251c6b103a3
dzemil karisik <karisikdzemil@gmail.com>
2025-07-10 19:10:58 +0200

    style

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,84 @@
+import Feather from "@expo/vector-icons/Feather";
 import { Tabs } from "expo-router";
+import { Text, View } from "react-native";
+
+const TabIcon = ({ focused, icon, title }: any) => {
+  return (
+    <View className="w-[50px] h-[50px] flex items-center gap-1">
+      <Feather name={icon} size={24} color={focused ? "orange" : "gray"} />
+      <Text className={focused ? "text-orange-400" : "text-gray-400"}>
+        {title}
+      </Text>
+    </View>
+  );
+
+  // return(
+  //   <View className="text-gray-500">
+  //       <Text>{icon}</Text>
+  //       <Text>{title}</Text>
+  //     </View>
+  // )
+};
 
 export default function RootLayout() {
-  return <Tabs>
-    <Tabs.Screen name="index" options={{title: "Home", headerShown: false}}/>
-    <Tabs.Screen name="categories" options={{title: "Categories", headerShown: false}}/>
-    <Tabs.Screen name="cart" options={{title: "Cart", headerShown: false}}/>
-    <Tabs.Screen name="profile" options={{title: "Profile", headerShown: false}}/>
-  </Tabs>;
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        // tabBarItemStyle: {
+        //   width: "100%",
+        //   height: "100%",
+        //   display: "flex",
+        //   justifyContent: "center",
+        //   alignItems: "center",
+        // },
+        tabBarStyle: {
+          backgroundColor: "#1C1C1E",
+          paddingTop: "20",
+          paddingBottom: "5",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon="home" title="Home" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="categories"
+        options={{
+          title: "Categories",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon="search" title="Search" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="cart"
+        options={{
+          title: "Cart",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon="search" title="Search" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon="home" title="Home" />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
