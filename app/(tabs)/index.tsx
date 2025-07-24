a82935e8f2f661790436b53ffd0aa7c32ee4caa9
dzemil karisik <karisikdzemil@gmail.com>
2025-07-24 17:46:48 +0200

    finishing project

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,56 +5,135 @@ import { fetchProducts } from "@/redux/slices/productsSlice";
 import { AppDispatch, RootState } from "@/redux/store";
 import { Product } from "@/types";
 import Feather from "@expo/vector-icons/Feather";
-import { useEffect } from "react";
+import { Link, useRouter } from "expo-router";
+import { useEffect, useState } from "react";
 import {
   ActivityIndicator,
   FlatList,
+  Modal,
   ScrollView,
   Text,
-  View
+  TouchableOpacity,
+  View,
 } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function Index() {
   const dispatch = useDispatch<AppDispatch>();
+  const [visible, setVisible] = useState(false);
   const {
     items: products,
     loading,
     error,
   } = useSelector((state: RootState) => state.products);
   const user = useSelector((state: RootState) => state.user);
-  
+  const router = useRouter();
+
   useEffect(() => {
     dispatch(fetchProducts());
   }, [dispatch]);
-  
+
   const featured = products.slice(0, 4);
 
+  const goToAddressHandler = () => {
+    router.push("/profile/ManageAddresses")
+    setVisible(false)
+  }
+   const goToPaymentHandler = () => {
+    router.push("/profile/PaymentMethods")
+    setVisible(false)
+  }
 
   return (
     <View style={{ backgroundColor: "#121212", flex: 1 }}>
       <ScrollView>
         <View className="bg-[#1C1C1E] mt-20 flex-row justify-between items-center p-5">
           <View className="flex-col gap-2">
-            <Text className="text-xl text-gray-400">Good Morning,</Text>
-            <Text className="text-2xl font-bold text-white">{user.fullName}</Text>
+            <Text className="text-xl text-gray-400">Welcome Back</Text>
+            <Text className="text-2xl font-bold text-white">
+              {user.fullName}
+            </Text>
           </View>
-           {/* <TouchableOpacity onPress={() => dispatch(clearUser())} >
-       <Text className="text-5xl text-white">Brisii</Text>
-      </TouchableOpacity> */}
-          <View className="flex-row gap-5">
-            <Feather
-              name="search"
-              size={32}
-              color="white"
-              className="p-2 bg-[#2f2e2e] rounded-xl"
-            />
-            <Feather
-              name="bell"
-              size={32}
-              color="white"
-              className="p-2 bg-[#2f2e2e] rounded-xl"
-            />
+          <View className="flex-row gap-5 relative">
+            <View className="p-4">
+              <Link
+                href="/(tabs)/categories"
+                className="p-2 bg-[#2f2e2e] rounded-xl"
+              >
+                <Feather name="search" size={32} color="white" />
+              </Link>
+            </View>
+
+            <View className="p-4">
+              <TouchableOpacity onPress={() => setVisible(true)}>
+                <Feather
+                  name="bell"
+                  size={32}
+                  color="white"
+                  className="p-2 bg-[#2f2e2e] rounded-xl"
+                />
+              </TouchableOpacity>
+
+              <Modal
+                animationType="slide"
+                transparent={true}
+                visible={visible}
+                onRequestClose={() => setVisible(false)}
+              >
+                <View className="flex-1 justify-center items-center bg-black/50">
+                  <View className="bg-[#1c1c1e] p-6 rounded-2xl w-80">
+                    <Text className="text-white text-xl font-bold mb-2">
+                      Message
+                    </Text>
+                    {user.paymentMethods.length === 0 ||
+                    Array.isArray(user.address) ? (
+                      <View className="bg-gray-300 p-4 rounded-xl mt-4">
+                        <Text className="text-red-800 font-semibold text-center mb-2">
+                          To make a purchase, you need to add your address and a
+                          payment method.
+                        </Text>
+                        <View className="flex-row justify-center gap-6">
+                          <TouchableOpacity
+                            onPress={goToAddressHandler}
+                          >
+                            <Text className="text-[#FF5C00] underline">
+                              Add Address
+                            </Text>
+                          </TouchableOpacity>
+                          <TouchableOpacity onPress={goToPaymentHandler}
+                          >
+                            <Text className="text-[#FF5C00] underline">
+                              Add Payment
+                            </Text>
+                          </TouchableOpacity>
+                        </View>
+                      </View>
+                    ) : (
+                      <View className="p-4 mt-4">
+                        <Text className="text-center text-gray-500">
+                          There are currently no messages.
+                        </Text>
+                      </View>
+                    )}
+
+                    <TouchableOpacity
+                      onPress={() => setVisible(false)}
+                      className="bg-[#FF5C00] rounded-xl p-3 mt-2"
+                    >
+                      <Text className="text-white text-center font-semibold">
+                        Close
+                      </Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              </Modal>
+            </View>
+            {(user.paymentMethods.length === 0 ||
+              Array.isArray(user.address)) && (
+              <Text className="text-white w-6 h-6 pt-0.5 text-center rounded-[50%] right-0 top-0 bg-red-600 absolute">
+                1
+              </Text>
+            )}
           </View>
         </View>
 
@@ -69,7 +148,7 @@ export default function Index() {
               keyExtractor={(item) => item.id.toString()}
               scrollEnabled={false}
               renderItem={({ item }) => (
-              <Item
+                <Item
                   id={item.id}
                   imageUrl={item.image}
                   title={item.title}
@@ -77,7 +156,7 @@ export default function Index() {
                   rating={item.rating}
                   price={item.price}
                 />
-          )}
+              )}
               numColumns={2}
               columnWrapperStyle={{
                 justifyContent: "space-between",
--- components/TitleSee.tsx
+++ components/TitleSee.tsx
@@ -1,9 +1,10 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 export default function TitleSee( {title}:any ) {
   return (
     <View className="flex-row items-center justify-between p-5">
       <Text className="text-2xl text-white font-bold">{title}</Text>
-      <Text className="text-base text-[#FF5C00]">See all</Text>
+      <Link href="/(tabs)/categories"><Text className="text-base text-[#FF5C00]">See all</Text></Link>
     </View>
   );
 }
